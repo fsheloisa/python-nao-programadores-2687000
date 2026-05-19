92f643f779b58becb6838f0b4f477a2feaa8142b
Heloísa Ferreira <helofs02@hotmail.com>
2026-05-19 13:29:27 +0000

    atualização exercicio

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -9,12 +9,14 @@
 
 
 # Declare uma variável do tipo booleano
-nome = Heloisa
-Objetivos = ''' aprender python
+
+objetivos = ''' aprender python
 ...pra mudar de profissão
 ...e conseguir um emprego remoto'''
 
 numero_inteiro = 7
 verdadeiro = True
 falso = False
+resumo = "Paloma é uma mulher de 46 anos que deseja mudar de profissão, por isso está estudando 'python'."
+
 
